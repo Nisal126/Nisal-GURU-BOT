03724119d70d0bdc8f63f5e2aca1f39a492cdf58
Nisal126 <148027558+Nisal126@users.noreply.github.com>
2023-10-16 14:05:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94701053003', 'me', true],
+  ['94701053003', 'Nisal', true], 
   [''] 
 ] //Number of owners
 
